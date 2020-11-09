7bd62195ecc50c0954f629baacd85dc4cc7fc24a
adhough <33616464+adhough@users.noreply.github.com>
2020-11-09 14:26:11 +0000

    image test 49

--- src/components/Values.js
+++ src/components/Values.js
@@ -15,17 +15,17 @@ function ValuesSection() {
     <br />
 
     <br />
-    <h3>Innovation</h3><p> : Passion for developing solutions which can improve businesses and lives.  </p>
+    <h3>Innovation</h3><p>Passion for developing solutions which can improve businesses and lives.  </p>
     <br />
-    <h3>Simplicity</h3><p> : Search for the most simple solutions to help improve understanding and reduce risk.  </p>
+    <h3>Simplicity</h3><p>Search for the most simple solutions to help improve understanding and reduce risk.  </p>
     <br />
-    <h3>Ethics</h3><p> : Approach business with high ethical principles and actively engage in discussions about Ethics within the Fintech community.  </p>
+    <h3>Ethics</h3><p>Approach business with high ethical principles and actively engage in discussions about Ethics within the Fintech community.  </p>
     <br />
-    <h3>Methodical Action</h3><p> : Take methodical and action oriented approaches to projects.  </p>
+    <h3>Methodical Action</h3><p>Take methodical and action oriented approaches to projects.  </p>
     <br />
-    <h3>Openness to Ideas</h3><p> : Welcome honest exchanges and be open to different persepectives and new ideas.  </p>
+    <h3>Openness to Ideas</h3><p>Welcome honest exchanges and be open to different persepectives and new ideas.  </p>
     <br />
-    <h3>First Principles</h3><p> : Question and challenge assumptions, break down problems to essential elements and develop 'bottom-up' perspectives.  </p>
+    <h3>First Principles</h3><p>Question and challenge assumptions, break down problems to essential elements and develop 'bottom-up' perspectives.  </p>
     <br />
     </div>
 
